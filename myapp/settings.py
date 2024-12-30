9f5e63cf485b2aff9a9770e7a0347121b0715047
sj-af <pssoorajkbfc@gmail.com>
2024-12-30 11:07:44 +0530

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 
