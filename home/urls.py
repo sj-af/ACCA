a0a8ff7395959666a60c4382982fb11c38417405
sj-af <pssoorajkbfc@gmail.com>
2024-12-31 14:23:51 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('login/',views.login,name='login'),
-    path('text/',views.text,name='text'),
+    path('text/',views.chat_room,name='text'),
     path('clear/',views.clear,name='clear'),
     path('register/',views.register,name='register')
     
